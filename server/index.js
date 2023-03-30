29a7a7f16bd7c3136392b8838a31e67caf32de26
rathikm <rmur3211@gmail.com>
2023-03-29 22:17:39 -0700

    day 2 of deployment - changing postbuild install order

--- server/index.js
+++ server/index.js
@@ -8,14 +8,12 @@ const app = express();
 
 const api = require("./api")
 
-if (process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(__dirname,"client", "build")))
 
-  app.use(express.static(path.join(__dirname,"client", "build")))
-  
-  app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname,"client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"client", "build", "index.html"));
+});
+
 app.use(cors())
 app.use('/api', api)
 
